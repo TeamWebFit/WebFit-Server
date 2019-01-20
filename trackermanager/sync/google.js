ef4174fe70f1522980790419c8ee4c26e7ce3472
Christian Weisenburger <christian.weisenburger@gmail.com>
2019-01-20 21:37:32 +0100

    Edit Email URL & Final Google Sync

--- trackermanager/sync/google.js
+++ trackermanager/sync/google.js
@@ -34,6 +34,7 @@ router.get('/sync/google', function (req, res) {
         id,
         access_token,
         token_type,
+        refreshtoken,
         trackerModel{
           id,
           apiLink,
@@ -57,6 +58,7 @@ router.get('/sync/google', function (req, res) {
       var apiLink = data['data'].tracker.trackerModel.apiLink // e.g. api.fitbit.com
       var apiLinkRequest = data['data'].tracker.trackerModel.apiLinkRequest // e.g. /steps
       var lastsyncdate = data['data'].tracker.lastSync
+      var refreshtoken = data['data'].tracker.refreshtoken
             // REQUEST CURRECT ACCESS_TOKEN
             // THROUGH AXIOS REFRESH_TOKEN REQUEST
             var refresh_link = apiLink + "/oauth2/v4/token"
@@ -68,7 +70,7 @@ router.get('/sync/google', function (req, res) {
 
                         client_id : "1008561982846-6omt5dknbiqv124o2h5g5nrgg27l7o7v.apps.googleusercontent.com",
                         client_secret : "qo4YWmhVsTn02kyIQkL-z0a0",
-                        refresh_token : "1/2G8LNrq95nIY7adRopmPyLZVDxfTt7ZDTdyNHRBu6dz_Zw7Vfow8KriRb2CTAcDN",
+                        refresh_token : refreshtoken,
                         grant_type : "refresh_token"
 
 
@@ -83,12 +85,11 @@ router.get('/sync/google', function (req, res) {
 
                     // ==== EIGENTLICHER API REQUEST ==== //
 
-                  if (apiLinkRequest === "/fitness/v1/users/me/dataset:aggregate") {
-
+                  
                     var step_request_link = apiLink + apiLinkRequest
                     var currentdate = new Date().getTime()
 
-                    // Check if first sync
+                     // Check if first sync
                     if (lastsyncdate === "9999"){
                       var starttime = currentdate - 1209600000
                     }else{
@@ -104,6 +105,16 @@ router.get('/sync/google', function (req, res) {
                       "startTimeMillis": starttime,
                       "endTimeMillis": currentdate
                     };
+
+                    const params2 = {
+                      "aggregateBy": [{
+                        "dataTypeName": "com.google.heart_rate.summary",
+                        "dataSourceId": "derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm"
+                      }],
+                      "bucketByTime": { "durationMillis": 60000 },
+                      "startTimeMillis": starttime,
+                      "endTimeMillis": currentdate
+                    }
 
                     axios.post(step_request_link, params, {
                       headers: {
@@ -132,20 +143,14 @@ router.get('/sync/google', function (req, res) {
                                 mutation{
                                   createSteps(
                                   time: "${time}",
-                                  value: "${steps_counter}",
+                                  value: ${steps_counter},
                                   trackerId: "${tracker}",
                                   userId: "${user}"
                                 ){
                                   time
                                 }
                                 }
-                                `).then(done => {
-                              query(`
-                                    mutation{
-                                      updateTracker(id: "${tracker}", lastSync: "${currentdate}"){id}
-                                      }
-                                  `)
-                            })
+                                `)
 
                             console.log(steps_counter)
                           }
@@ -156,8 +161,66 @@ router.get('/sync/google', function (req, res) {
                       res.send("Error #05 - Warehouse-API returned an error <br />" + error)
                     })
 
+             //Heart Rate
 
+             axios.post(step_request_link, params2, {
+              headers: {
+                  'content-type': 'application/json',
+                  'Authorization': token_type + " " + access_token
+              },
+            })
+            .then(function (response) {
+              console.log("========== Heart Rate ============")
+              //console.log(response.data)
+              var heartrate = response.data.bucket
+
+              heartrate.forEach(rate => {
+                //console.log(element.dataset[0].point)
+                  if (rate.dataset[0].point.length === 0){
+
+                  }else{
+                    var bucket_with_rate = rate.dataset[0].point[0]
+                   //console.log(bucket_with_rate)
+                    var bucket_start_time_rate = bucket_with_rate.startTimeNanos
+                    var bucket_end_time = bucket_with_rate.endTimeNanos
+                    var rate_counter = bucket_with_rate.value[0].fpVal
+                    var time_rate = Math.round(bucket_start_time_rate / 1000000)
+                    console.log(time_rate + " " + rate_counter + " " + tracker + user)
+
+                    query(`
+                        mutation{
+                          createHeartRate(
+                          time: "${time_rate}",
+                          value: ${rate_counter},
+                          trackerId: "${tracker}",
+                          userId: "${user}"
+                        ){
+                          time
+                        }
+                        }
+                        `).then(done => {
+                          console.log("Hat funktioniert")
+                          console.log(done)
+                      query(`
+                            mutation{
+                              updateTracker(id: "${tracker}", lastSync: "${currentdate}"){id}
+                              }
+                          `)
+                    }).catch(error => {
+                      console.log(error)
+                    })
+
+                    console.log(rate_counter)
                   }
+              })
+            })
+            .catch(function (error) {
+              console.log(error)
+              res.send("Error #05 - Warehouse-API returned an error <br />" + error)
+            })
+
+
+                  
 
                     // ==== EIGENTLICHER API REQUEST ENDE ==== //
 
